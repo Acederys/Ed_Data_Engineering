606a1470918edd6edad33a4564e76d9680063779
Alexandra <acederys@mail.ru>
2023-11-26 12:56:50 +0300

    Добавлено решение для пя5й задачи

--- data_ingeneering/4_main_4.py
+++ data_ingeneering/4_main_4.py
@@ -27,13 +27,6 @@ def parce_data(data_1):
             if i == '=====\n':
                 item = dict()
                 item['category'] = 'no'
-                item['name'] = 'no'
-                item['price'] = 'no'
-                item['quantity'] = 'no'
-                item['fromCity'] = 'no'
-                item['isAvailable'] = 'no'
-                item['views'] = 'no'
-                item['version'] = 0
                 items.append(item)
             else:
                 i = i.strip()
@@ -44,11 +37,62 @@ def parce_data(data_1):
                     item[splitted[0]] = float(splitted[1])
                 else:
                     item[splitted[0]] = splitted[1]
-    clear_item = []
-    for elem in items:
-        if elem['name'] != 'no':
-            clear_item.append(elem)
-    return clear_item
+    clear_items = []
+    for i in items:
+        if i.get('name', False) != False:
+            clear_items.append(i)
+    return clear_items
+item_1 = parce_data('task_4_var_07_product_data.text')
+# print(len(item_1))
+# print(item_1)
+
+# подключаемся к базе данных
+def connect_to_db(elem):
+    connection = sqlite3.connect(elem)
+    connection.row_factory = sqlite3.Row
+    return connection
+db = connect_to_db('4_1_db.db')
+
+def insert_price(db, data):
+    cursor = db.cursor()
+    cursor.executemany("""
+        INSERT INTO products (name, price, quantity, category, fromCity, isAvailable, views)
+        VALUES(
+        :name, :price, :quantity, :category, :fromCity, :isAvailable, :views
+        )
+    """, data)
+    db.commit()
+
+# insert_price(db, item_1)
+
+def delete_by_name(db, name):
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM products WHERE name = ?', [name])
+    db.commit()
+def update_price_by_precent(db, name, precent):
+    cursor = db.cursor()
+    cursor.execute('UPDATE products SET price = ROUND((price * (1 + ?)), 2) WHERE name = ?', [precent, name])
+    cursor.execute('UPDATE products SET version = version +1 WHERE  name = ?', [name])
+    db.commit()
+def quantity_add(db, name, quantity):
+    cursor = db.cursor()
+    res = cursor.execute('UPDATE products SET quantity = (quantity + ?) WHERE (name = ?) AND ((quantity + ?)> 0)', [quantity, name, quantity])
+    if res.rowcount > 0:
+        cursor.execute('UPDATE products SET version = version +1 WHERE  name = ?', [name])
+        db.commit()
+def price_abs(db, name, value):
+    cursor = db.cursor()
+    res = cursor.execute('UPDATE products SET price = (price + ?) WHERE (name = ?) AND ((price + ?)> 0)', [value, name, value])
+    if res.rowcount > 0:
+        cursor.execute('UPDATE products SET version = version +1 WHERE  name = ?', [name])
+        db.commit()
+def available(db, name, value):
+    cursor = db.cursor()
+    res = cursor.execute('UPDATE products SET isAvailable = ? WHERE (name = ?)', [value, name])
+    cursor.execute('UPDATE products SET version = version +1 WHERE  name = ?', [name])
+    db.commit()
+
+
 def parce_data_mp(data_2):
     with open(data_2, 'rb') as mp_file:
         data_mp = msgpack.load(mp_file)
@@ -58,117 +102,32 @@ def parce_data_mp(data_2):
         elif elem['method'] == 'available' and elem['param'] == False:
             elem['param'] = 'False'
     return data_mp
-
-# подключаемся к базе данных
-def connect_to_db(elem):
-    connection = sqlite3.connect(elem)
-    connection.row_factory = sqlite3.Row
-    return connection
-
-def insert_price(db, data):
-    cursor = db.cursor()
-    cursor.executemany("""
-        INSERT INTO products_1 (name, price, quantity, category, fromCity, isAvailable, views, version)
-        VALUES(
-        :name, :price, :quantity, :category, :fromCity, :isAvailable, :views, :version
-        )
-    """, data)
-    db.commit()
-
-item_1 = parce_data('task_4_var_07_product_data.text')
 item_2 = parce_data_mp('task_4_var_07_update_data.msgpack')
-db = connect_to_db('4_1_db.db')
-# заносим данные в бд
-insert_price(db, item_1)
-def delete_be_name(db, name):
-    cursor = db.cursor()
-    cursor.executemany("""
-        DELETE FROM products_1 WHERE name = ?
-    """, [name])
-    db.commit()
-def update_price(db, name, percent):
-    cursor = db.cursor()
-    sql_update_query = """UPDATE products_1 SET price = ? WHERE name = ?"""
-    data = (percent,name)
-    cursor.execute(sql_update_query, data)
-    db.commit()
-    cursor.close()
-def quantity_add(db, name, percent):
-    cursor = db.cursor()
-    sql_update_query = """UPDATE products_1 SET quantity = quantity + ? WHERE name = ?"""
-    data = (percent,name)
-    cursor.execute(sql_update_query, data)
-    db.commit()
-    cursor.close()
-def available(db, name, percent):
-    cursor = db.cursor()
-    sql_update_query = """UPDATE products_1 SET isAvailable =  ? WHERE name = ?"""
-    data = (percent,name)
-    cursor.execute(sql_update_query, data)
-    db.commit()
-    cursor.close()
-def price_abs(db, name, percent):
-    cursor = db.cursor()
-    sql_update_query = """UPDATE products_1 SET price = price + ? WHERE name = ?"""
-    data = (percent,name)
-    cursor.execute(sql_update_query, data)
-    db.commit()
-    cursor.close()
-def update_version(db, name):
-    cursor = db.cursor()
-    sql_update_query = """UPDATE products_1 SET version = version+? WHERE name = ?"""
-    data = (1, name)
-    cursor.execute(sql_update_query, data)
-    db.commit()
-    cursor.close()
 
-# скачиваем бд
-def downloads_db(db):
-    cursor = db.cursor()
-    result = cursor.execute("""
-    SELECT *
-        FROM products_1
-    """,)
-    data = []
-    for row in result.fetchall():
-        elem = dict(row)
-        data.append(elem)
-    cursor.close()
-    return data
-# обновляем данные в бд
-def third_query(db, update_items, data):
-    products = dict()
-    data_json = data
-    for elem in data_json:
-        products[elem['name']] = elem
-    print(data)
-    # Ищем элемент файле с ценами
-    for elem in update_items:
-        # соединяем имена из двух словарей
-        item = products[elem['name']]
-        if elem['method'] == 'price_abs':
-            res = item['price'] + elem['param']
-            if res > 0:
-                price_abs(db, item['name'], elem['param'])
-                update_version(db, item['name'])
-        elif elem['method'] == 'price_percent':
-            res = item['price'] + (item['price']*elem['param'])
-            if res > 0:
-                update_price(db, item['name'], res)
-                update_version(db, item['name'])
-        elif elem['method'] == 'quantity_add':
-            res = item['quantity'] + elem['param']
-            if res > 0:
-                quantity_add(db, item['name'], elem['param'])
-                update_version(db, item['name'])
-        elif elem['method'] == 'available':
-            available(db, item['name'], elem['param'])
-            update_version(db, item['name'])
-        elif elem['method'] == 'remove':
-            delete_be_name(db, [item['name']])
-data = downloads_db(db)
-# print(len(data))
-third_query(db, item_2, data)
+def hungle_update(db, update_items):
+    for item in update_items:
+        match item['method']:
+            case 'remove':
+                print(f'Удалить {item["name"]}')
+                delete_by_name(db, item["name"])
+            case 'price_percent':
+                print(f"Изменить на процент {item['name']} {item['param']}")
+                update_price_by_precent(db, item['name'], item['param'])
+            case 'price_abs':
+                print(f"Изменение цены {item['name']} {item['param']}")
+                price_abs(db, item['name'], item['param'])
+            case 'available':
+                print(f"Изменение доступности {item['name']} {item['param']}")
+                price_abs(db, item['name'], item['param'])
+            case 'quantity_add':
+                print(f"Изменение количества {item['name']} {item['param']}")
+                quantity_add(db, item['name'], item['param'])
+            case 'quantity_sub':
+                print(f"Изменение количества {item['name']} {item['param']}")
+                price_abs(db, item['name'], item['param'])
+
+
+# hungle_update(db, item_2)
 
 #вывести топ-10 самых обновляемых товаров
 
@@ -176,7 +135,7 @@ def first_query(db, limit):
     cursor = db.cursor()
     result = cursor.execute("""
         SELECT *
-        FROM products_1
+        FROM products
         ORDER BY version DESC
         LIMIT ?
         """,[limit])
@@ -191,7 +150,7 @@ first_query(db, 10)
 def min_max(db):
     cursor = db.cursor()
     result = cursor.execute("""
-        SELECT 
+        SELECT
             SUM(price) as sum_price,
             AVG(price) as avg_price,
             MIN(price) as min_price,
@@ -204,7 +163,7 @@ def min_max(db):
             AVG(views) as avg_views,
             MIN(views) as min_views,
             MAX(views) as max_views
-        FROM products_1
+        FROM products
         """)
     print(dict(result.fetchone()))
     cursor.close()
@@ -216,8 +175,8 @@ min_max(db)
 def anasis_quality(db):
     cursor = db.cursor()
     result = cursor.execute("""
-        SELECT category, AVG(quantity) as avg_price 
-        FROM products_1
+        SELECT category, AVG(quantity) as avg_price
+        FROM products
         GROUP BY category
         """)
     print(dict(result.fetchall()))
@@ -230,10 +189,10 @@ anasis_quality(db)
 def second_query(db):
     cursor = db.cursor()
     result = cursor.execute("""
-        SELECT 
+        SELECT
             COUNT(*) as count,
             fromCity as fromCity
-            FROM products_1
+            FROM products
             GROUP BY fromCity
         """)
     items = []
@@ -244,5 +203,3 @@ def second_query(db):
     print(items)
 
 second_query(db)
-
-
--- data_ingeneering/4_main_5.py
+++ data_ingeneering/4_main_5.py
@@ -28,32 +28,180 @@
 #
 # скрипт с выполнением запросов к базе данных  
 import csv
+import json
 import pandas as pd
 import sqlite3
-df = pd.read_csv('df_2009.csv')
-clear_df = df.drop_duplicates(subset=['Address'])
-clear_df.rename(columns = {'List Year':' ListYear', 'Date Recorded':'DateRecorded', 'Assessed Value':'AssessedValue', 'Sale Amount':'SaleAmount', 'Property Type': 'PropertyType', 'Residential Type':'ResidentialType'}, inplace = True )
-clear_df.insert(loc= len(clear_df.columns) , column='version', value=0)
-
-#подключаемся к базе данных
+# загрузжаем данне
+with open('apartment.csv') as file:
+    data_csv = csv.reader(file)
+    next(data_csv)
+    data = []
+    for i in data_csv:
+        # print(i)
+        item = {
+            'ListYear': int(i[0]),
+            'Address': i[2],
+            'SaleAmount': float(i[4]),
+            'PropertyType': i[5]
+        }
+        data.append(item)
+item_1 = data.copy()
+# подключаемся к БД
 def connect_to_db(elem):
     connection = sqlite3.connect(elem)
     connection.row_factory = sqlite3.Row
     return connection
+db = connect_to_db('4_5_db.db')
 
-# def insert_price(db, data):
-#     cursor = db.cursor()
-#     cursor.executemany("""
-#         INSERT INTO apartament (ListYear, DateRecorded, Town, Address, AssessedValue, SaleAmount, PropertyType, ResidentialType, version)
-#         VALUES(
-#         :ListYear, :DateRecorded, :Town, :Address, :AssessedValue, :SaleAmount, :PropertyType, :ResidentialType, :version
-#         )
-#     """, data)
-#     db.commit()
-db = connect_to_db('4_1_db.db')
-# insert_price(db, clear_df)
-df.to_sql("apartament", con=con, if_exists="append", index=False)
-# pd.read_sql('''
-#     SELECT *
-#     FROM user
-# ''', con)
+def insert_price(db, data):
+    cursor = db.cursor()
+    cursor.executemany("""
+        INSERT INTO apartament (ListYear, Address, SaleAmount, PropertyType)
+        VALUES(
+        :ListYear, :Address, :SaleAmount, :PropertyType
+        )
+    """, data)
+    db.commit()
+
+# insert_price(db, item_1)
+
+df = pd.read_json('AssessedValue.json')
+item_2 = df.to_dict('records')
+def insert_AssessedValue(db, data):
+    cursor = db.cursor()
+    cursor.executemany("""
+        INSERT INTO AsessedValue (id_AsessedValue, Address, AssessedValue)
+        VALUES(
+            (SELECT id FROM apartament WHERE Address = :Address),
+        :Address, :AssessedValue
+        )
+    """, data)
+    db.commit()
+
+# insert_AssessedValue(db, item_2)
+
+df = pd.read_csv('town.csv')
+item_3 = df.to_dict('records')
+print(len(item_3))
+def insert_town(db, data):
+    cursor = db.cursor()
+    cursor.executemany("""
+        INSERT INTO town (id_town, Town, Address)
+        VALUES(
+            (SELECT id FROM apartament WHERE Address = :Address),
+        :Town, :Address
+        )
+    """, data)
+    db.commit()
+
+# insert_town(db, item_3)
+
+def first_query(db, limit):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT *
+        FROM apartament
+        WHERE SaleAmount > 10000
+        ORDER BY PropertyType DESC
+        LIMIT ?
+        """,[limit])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    # with open(f'SaleAmount_10000.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+first_query(db, 100)
+
+
+# подсчет объектов по условию
+
+def second_query(db, name):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT *
+        FROM town
+        WHERE id_town = (SELECT id FROM apartament WHERE PropertyType = ?)
+        """, [name])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    # with open(f'Residential.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+
+second_query(db, 'Residential')
+
+def therr_query(db, name):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT *
+        FROM apartament
+        WHERE id = (SELECT id_town FROM town WHERE Town = ?)
+        """, [name])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    # with open(f'Ansonia.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+therr_query(db, 'Ansonia')
+
+
+def foir_query(db, name):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT *
+        FROM AsessedValue
+        WHERE AssessedValue > 10000
+        LIMIT ?
+        """, [name])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    # with open(f'AssessedValue_10000.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+
+foir_query(db, 10)
+
+def five_query(db, name):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT 
+            AVG(AssessedValue) as avg_AssessedValue,
+            *
+        FROM AsessedValue
+        WHERE id_AsessedValue = (SELECT id FROM town WHERE Town = ?)
+        """, [name])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    print(items)
+    # with open(f'avg_AssessedValue.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+
+five_query(db, 'Ansonia')
+
+def six_query(db, name):
+    cursor = db.cursor()
+    result = cursor.execute("""
+        SELECT *
+        FROM apartament
+        WHERE id = (SELECT id_AsessedValue FROM AsessedValue  WHERE AssessedValue < ?)
+        """, [name])
+    items = []
+    for row in result.fetchall():
+        item = dict(row)
+        items.append(item)
+    cursor.close()
+    # with open(f'AssessedValue_9999999.json', 'w', encoding='utf-8') as file_1:
+    #     file_1.write(json.dumps(items))
+
+six_query(db, 9999999)
